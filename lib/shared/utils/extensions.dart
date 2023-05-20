9507bcf5e3f4fbb1b81a82e1351593bcf1d13f94
carlosctu <carlosctu94@gmail.com>
2023-05-20 19:56:25 -0300

    feat: adding extensions for String and int

---
+++ lib/shared/utils/extensions.dart
@@ -0,0 +1,11 @@
+extension StringExtension on String {
+  String capitalizeFirstLetter() {
+    return this[0].toUpperCase() + substring(1);
+  }
+}
+
+extension NumberExtension on int {
+  String formatPokemonNumber() {
+    return '#${toString().padLeft(3, '0')}';
+  }
+}
